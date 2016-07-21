3f2b2417583af3d74d234419f6280bf0c2045dd8
guanda <david.guan@pioneer.com>
2016-07-21 12:48:04 -0500

    Trying to get this to work on button click, not working yet

--- background.js
+++ background.js
@@ -1,6 +1,22 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
+    console.log(tab);
+    
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        console.log(tabs);
         var activeTab = tabs[0];
         chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
-  });
+    });
+    if (tab.url.indexOf("https://www.messenger.com") != -1) {
+        // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        //     console.log(tabs);
+        //     var activeTab = tabs[0];
+        //     chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+        // });
+        console.log("in here");
+        chrome.tabs.executeScript(tab.id, {
+            "file": "content.js"
+        }, function () {
+            console.log("script executed");
+        });
+    }
 });
